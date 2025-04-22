e25bfff7121e3d0e2b625655025a314e705a4260
이동휘 <164876151+dongwhi09@users.noreply.github.com>
2025-04-22 12:45:50 +0900

    [Silver IV] Title: 큐, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

---
+++ Python/백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,28 @@
+import sys
+l = []
+for i in range(int(sys.stdin.readline())):
+    a = list(map(str, sys.stdin.readline().split()))
+    if len(a) == 2:
+        l.append(a[1])
+    if a[0] == "pop":
+        if l:
+            print(l.pop(0))
+        else:
+            print(-1)
+    if a[0] == "size":
+        print(len(l))
+    if a[0] == "empty":
+        if l:
+            print(0)
+        else:
+            print(1)
+    if a[0] == "front":
+        if l:
+            print(l[0])
+        else:
+            print(-1)
+    if a[0] == "back":
+        if l:
+            print(l[-1])
+        else:
+            print(-1)
