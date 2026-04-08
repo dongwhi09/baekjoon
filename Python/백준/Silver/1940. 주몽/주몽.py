8df51a42bea4009a24564d45feaa0a754022750f
이동휘 <164876151+dongwhi09@users.noreply.github.com>
2026-04-08 16:46:48 +0900

    [Silver IV] Title: 주몽, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- Python/백준/Silver/1940. 주몽/주몽.py
+++ Python/백준/Silver/1940. 주몽/주몽.py
@@ -2,7 +2,7 @@ n = int(input())
 m = int(input())
 l = list(map(int, input().split()))
 l.sort()
-left, right, count = 0, len(l) - 1, 0
+left, right, count = 0, n - 1, 0
 while left < right:
     if l[left] + l[right] < m:
         left += 1
