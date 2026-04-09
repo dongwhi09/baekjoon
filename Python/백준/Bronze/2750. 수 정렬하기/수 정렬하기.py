4145c8a5bd4fa8502e180a9d0d9b81133cac1bea
이동휘 <164876151+dongwhi09@users.noreply.github.com>
2026-04-09 17:42:13 +0900

    [Bronze II] Title: 수 정렬하기, Time: 208 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ Python/백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -2,6 +2,9 @@ n = int(input())
 l = []
 for i in range(n):
     l.append(int(input()))
-l.sort()
-for i in l:
-    print(i)
+for i in range(n - 1):
+    for j in range(n - 1 - i):
+        if l[j] > l[j + 1]:
+            l[j], l[j + 1] = l[j + 1], l[j]
+for i in range(n):
+    print(l[i])
