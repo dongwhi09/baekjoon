e7cc91cab7545fdea204447c9bf1a3ba506369df
이동휘 <164876151+dongwhi09@users.noreply.github.com>
2026-04-09 19:13:58 +0900

    [Silver V] Title: 소트인사이드, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ Python/백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,6 +1,10 @@
-n, arr = input(), []
-for i in n:
-    arr.append(i)
-arr.sort(reverse=True)
-for i in arr:
-    print(i, end='')
+n = list(input())
+for i in range(len(n)):
+    Max = i
+    for j in range(i + 1, len(n)):
+        if n[j] > n[Max]:
+            Max = j
+    if n[i] < n[Max]:
+        n[i], n[Max] = n[Max], n[i]
+for i in range(len(n)):
+    print(n[i], end='')
