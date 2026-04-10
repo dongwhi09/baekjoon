1f56d6ede3fd81df43a4afbec60216dc61026f4f
이동휘 <164876151+dongwhi09@users.noreply.github.com>
2026-04-10 10:32:48 +0900

    [Silver IV] Title: ATM, Time: 160 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/11399. ATM/ATM.py
+++ Python/백준/Silver/11399. ATM/ATM.py
@@ -1,8 +1,22 @@
 n = int(input())
 l = list(map(int, input().split()))
-l.sort(reverse = True)
-x = 0
-for i in range(n):
-    x += sum(l)
-    l[i] = 0
-print(x)
+s = [0] * n
+for i in range(1, n):
+    insert_point = i
+    insert_value = l[i]
+    for j in range(i - 1, -1, -1):
+        if l[j] < l[i]:
+            insert_point = j + 1
+            break
+        if j == 0:
+            insert_point = 0
+    for j in range(i, insert_point, -1):
+        l[j] = l[j - 1]
+    l[insert_point] = insert_value
+s[0] = l[0]
+for i in range(1, n):
+    s[i] = s[i - 1] + l[i]
+result = 0
+for i in s:
+    result += i
+print(result)
